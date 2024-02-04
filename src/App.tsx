3bda6fb5a90057cfd769993716150354ecf5ff65
guerlBlanchard <guerlain.blanchard@gmail.com>
2024-02-04 11:46:33 +0100

    [Add] theme provider for mui components

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { RouterProvider } from 'react-router-dom';
 import router from './routes/routes';
 import SnackBarProvider from './context/snackbar/SnackBarProvider';
 import UserProvider from './context/user/UserProvider';
+import { ThemeProvider } from '@mui/material';
+import { MuiTheme } from './theme/theme';
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={MuiTheme}>
       <UserProvider>
         <SnackBarProvider>
           <RouterProvider router={router}/>
         </SnackBarProvider>
       </UserProvider>
-    </div>
+    </ThemeProvider>
   );
 }
 
